eea95ef05d9c0295031b12e6c8a80f8b99f38fe4
OAmor <o.amor97@gmail.com>
2020-03-31 23:21:21 +0300

    add footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>CORONA</title>
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-            <!-- Font Awesome Icons -->
+        <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;900&display=swap" rel="stylesheet"> 
+        <!-- Font Awesome Icons -->
         <link rel="stylesheet" href="{{asset('css/fa_all.min.css')}}">
         <!-- Theme style -->
         <link href="{{ asset('css/toastr.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/mdb.min.css') }}" rel="stylesheet">
         <link href="{{ asset('css/main.css') }}" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;900&display=swap" rel="stylesheet"> 
     </head>
     <body>
         <header>
@@ -24,8 +25,24 @@
             </nav>
         </header>
 
+
+
+        <!-- Footer -->
+        <footer class="page-footer font-small blue">
+
+            <!-- Copyright -->
+            <div class="footer-copyright text-center py-3">© 2020 جميع الحقوق محفوظة
+            <a href="/">CORONA</a>
+            </div>
+            <!-- Copyright -->
+
+        </footer>
+        <!-- Footer -->
+
         <script src="{{asset('js/jquery.min.js')}}"></script>
-        <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{asset('js/popper.min.js')}}"></script>
+        <script src="{{asset('js/bootstrap.min.js')}}"></script>
+        <script src="{{ asset('js/mdb.min.js') }}"></script>
         <script src="{{ asset('js/main.js') }}"></script>
     </body>
 </html>
